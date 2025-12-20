ef8dba872dea4a8f13f43be5707efb948790c83e
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-12-20 22:03:40 +0100

    chore: update gitignore and clean up stacks.ts

--- bitlot-web/lib/stacks.ts
+++ bitlot-web/lib/stacks.ts
@@ -1,6 +1,8 @@
 import { AppConfig, UserSession, showConnect } from '@stacks/connect';
 import { StacksMainnet, StacksTestnet, StacksMocknet } from '@stacks/network';
 
+
+
 export const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const userSession = new UserSession({ appConfig });
 
